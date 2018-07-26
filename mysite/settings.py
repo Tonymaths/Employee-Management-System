49738d400917cdae21e7a00b4adaa1b5cde61cef
Tonymaths <41554748+Tonymaths@users.noreply.github.com>
2018-07-26 16:15:57 +0100

    .

--- mysite/settings.py
+++ mysite/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'fi9gp%*ucfc&9wal-b8ue@icdw%wf-vgq##mu-^5&qp*g)skdl'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
